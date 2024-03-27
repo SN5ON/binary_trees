e3a1108b1e25cb47e3c96e4c8bfc4172b37e4e35
SN5ON <shalomnyalila@gmail.com>
2024-03-27 14:42:51 -0700

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,7 +6,7 @@
  *
  * Return: If tree is NULL, your function must return 0, else return the depth.
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0;
 
