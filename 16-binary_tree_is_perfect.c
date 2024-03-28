24a50d1465f096185c67b17427168fc1efa655dc
SN5ON <shalomnyalila@gmail.com>
2024-03-27 18:07:10 -0700

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -67,7 +67,7 @@ int is_perfect_recursive(const binary_tree_t *tree,
  * @tree: a pointer to the root node of the tree to check
  *
  * Return: If tree is NULL, your function must return 0
- * 
+ *
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
